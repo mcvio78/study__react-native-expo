f5e687677b4d0ced1fa669be685881c04a58426d
mauro <mauro@joyspot-solutions.com>
2021-08-12 22:16:25 +0200

    DoneWithIt - WelcomeScreen

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+export const WelcomeScreen = () => {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.header}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
