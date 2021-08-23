8aac5fa631ca3ad0fcd0fd474ecceacd1378a0dc
mauro <mauro@joyspot-solutions.com>
2021-08-23 15:27:18 +0200

    DoneWithIt - MessagesScreen

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import { LoginScreen } from "./app/screens/LoginScreen";
 import { Card } from "./app/components/Card";
 import { ListingDetailsScreen } from "./app/screens/ListingDetailsScreen";
 import { ViewImageScreen } from "./app/screens/ViewImageScreen";
+import { MessagesScreen } from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
