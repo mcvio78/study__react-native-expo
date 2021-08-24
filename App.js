5c7a3ff7bd979a618f26d4b9ca18b038b2b85cff
mauro <mauro@joyspot-solutions.com>
2021-08-25 00:16:27 +0200

    DoneWithIt - cleaning

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { View } from "react-native";
 
 import { WelcomeScreen } from "./app/screens/WelcomeScreen";
 import { LoginScreen } from "./app/screens/LoginScreen";
-import { Card } from "./app/components/Card";
 import { ListingDetailsScreen } from "./app/screens/ListingDetailsScreen";
 import { ViewImageScreen } from "./app/screens/ViewImageScreen";
 import { MessagesScreen } from "./app/screens/MessagesScreen";
