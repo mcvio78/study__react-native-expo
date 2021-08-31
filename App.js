89e45469dc6753e94b5f6b7371e69a4fe82e238e
mauro <mauro@joyspot-solutions.com>
2021-08-31 10:22:57 +0200

    DoneWithIt - create a reusable Icon component

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ import { LoginScreen } from "./app/screens/LoginScreen";
 import { ListingDetailsScreen } from "./app/screens/ListingDetailsScreen";
 import { ViewImageScreen } from "./app/screens/ViewImageScreen";
 import { MessagesScreen } from "./app/screens/MessagesScreen";
+import { Screen } from "./app/components/Screen";
+import { Icon } from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={80} backgroundColor="red" iconColor="violet" />
+    </Screen>
+  );
 }
