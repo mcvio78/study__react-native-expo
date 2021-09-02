ae6c31d6e50a16066a3f6bbae309872f26e28ff3
maurovio <mcvio78@gmail.com>
2021-09-02 11:02:47 +0200

    DoneWithIt - refine ListingScreen

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
-
-import { WelcomeScreen } from "./app/screens/WelcomeScreen";
-import { LoginScreen } from "./app/screens/LoginScreen";
-import { ListingDetailsScreen } from "./app/screens/ListingDetailsScreen";
-import { ViewImageScreen } from "./app/screens/ViewImageScreen";
-import { MessagesScreen } from "./app/screens/MessagesScreen";
 import { Screen } from "./app/components/Screen";
-import { Icon } from "./app/components/Icon";
-import { ListItem } from "./app/components/ListItem";
-import { AccountScreen } from "./app/screens/AccountScreen";
-import { ListingScreen } from "./app/screens/ListingScreen";
+import { TextInput } from "react-native";
 
 export default function App() {
-  return <ListingScreen />;
+  return (
+    <Screen>
+      <TextInput placeholder="First Name" />
+    </Screen>
+  );
 }
--- app/components/Screen.js
+++ app/components/Screen.js
@@ -4,7 +4,11 @@ import { StyleSheet, SafeAreaView, View } from "react-native";
 import Constants from "expo-constants";
 
 export const Screen = ({ children, style }) => {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
