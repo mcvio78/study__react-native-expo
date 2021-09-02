05c474cdbdcad15d5805688a87eb896f91daf548
maurovio <mcvio78@gmail.com>
2021-09-02 12:02:23 +0200

    DoneWithIt - fix ListingScreen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 import Constants from "expo-constants";
 
 export const Screen = ({ children, style }) => {
-  return (
-    <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>{children}</View>
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
