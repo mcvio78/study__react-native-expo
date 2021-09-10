171b51db60b2769f489d8e4c5c4f1949e8fe2100
maurovio <mcvio78@gmail.com>
2021-09-10 15:32:24 +0200

    DoneWithIt - modify Screen for iOS (SafeAreaView paddingOrizontal)

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -5,8 +5,10 @@ import Constants from "expo-constants";
 
 export const Screen = ({ children, style }) => {
   return (
-    <SafeAreaView style={styles.screen}>
-      <View style={style}>{children}</View>
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={[styles.view, style]}>
+        {children}
+      </View>
     </SafeAreaView>
   );
 };
@@ -14,6 +16,9 @@ export const Screen = ({ children, style }) => {
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
     flex: 1,
   },
 });
