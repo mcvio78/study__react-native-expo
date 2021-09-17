8ea3bda3b4e0096a70734209a392ba742fa37154
maurovio <mcvio78@gmail.com>
2021-09-17 14:06:17 +0200

    DoneWithIt - install react navigation stack

--- App.jsx
+++ App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { ListingEditScreen } from './app/screens/ListingEditScreen';
+import { AppText } from './app/components/AppText';
+
+const Tweets = () => {
+  return <AppText>Tweets main page</AppText>;
+};
+
+const TweetsDetails = () => {
+  return <AppText>Tweets details</AppText>;
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="tweets" component={Tweets} />
+    <Stack.Screen name="tweetsDetails" component={TweetsDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
