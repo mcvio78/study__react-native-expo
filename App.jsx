830de3b4268029b9c169a98d541fd2bcbd5ca5ff
maurovio <mcvio78@gmail.com>
2021-09-20 11:49:50 +0200

    DoneWithIt - custom navigation theme

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
 import { AuthNavigator } from './app/components/forms/navigation/authNavigator';
+import { customTheme } from './app/components/forms/navigation/navigarionTheme';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={customTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
