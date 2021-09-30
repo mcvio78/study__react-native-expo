7b2301d953815d76120cfe76cc80adb70dfb03e9
maurovio <mcvio78@gmail.com>
2021-09-30 11:04:18 +0200

    DoneWithIt - use netInfo hook

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 // import { AuthNavigator } from './app/components/forms/navigation/authNavigator';
 import { TabNavigator } from './app/navigation/tabNavigator';
@@ -7,14 +8,17 @@ import { customTheme } from './app/navigation/theme/navigarionTheme';
 
 export default function App() {
   // Reactotron
-  /* eslint-disable-next-line */
   if (__DEV__) {
     import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
   }
 
+  const netInfo = useNetInfo();
+
   return (
-    <NavigationContainer theme={customTheme}>
-      <TabNavigator />
-    </NavigationContainer>
+    netInfo && (
+      <NavigationContainer theme={customTheme}>
+        <TabNavigator />
+      </NavigationContainer>
+    )
   );
 }
