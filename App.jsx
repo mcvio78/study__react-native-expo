e91803d45145f7ca0448458abb306c71b7be4b02
maurovio <mcvio78@gmail.com>
2021-10-11 11:57:31 +0200

    DoneWithIt - controlling the splash screen

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import jwtDecode from 'jwt-decode';
+import AppLoading from 'expo-app-loading';
 
 import { AuthNavigator } from './app/navigation/authNavigator';
 import { TabNavigator } from './app/navigation/tabNavigator';
@@ -11,18 +12,29 @@ import { authStorage } from './app/auth/storage';
 
 export default function App() {
   const [user, setUser] = useState();
-
-  useEffect(() => {
-    (async () => {
-      const token = await authStorage.getToken();
-      if (!token) return;
-      setUser(jwtDecode(token));
-    })();
-  }, []);
+  const [isReady, setIsReady] = useState(false);
 
   // Reactotron
   if (__DEV__) {
     import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+  }
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+    setUser(jwtDecode(token));
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={(error) => {
+          console.log('Error occurred: ', error);
+        }}
+      />
+    );
   }
 
   return (
